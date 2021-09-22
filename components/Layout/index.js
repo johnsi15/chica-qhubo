225b8d3a159794c137a136db979f28d23479f292
John Serrano <andrey_201015@hotmail.com>
2021-09-22 16:42:59 -0500

    Add styles version mobile

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -7,6 +7,11 @@ export default function Layout({ children }) {
       <Head>
         <title>Chica Qhubo</title>
         <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1,minimum-scale=1, maximum-scale=5'
+        />
+        <meta name='description' content='Chica Qhubo Cúcuta' />
       </Head>
       <main className={styles.main}>{children}</main>
     </>
