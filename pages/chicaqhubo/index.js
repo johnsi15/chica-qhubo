868d9d896acfedb0ee6c54759bb596c843e8a834
Camilo Galvis <deiby.galvis@laopinion.com.co>
2022-01-05 08:32:43 -0500

    modificando url btn ver chica qhubo del dia

--- pages/chicaqhubo/index.js
+++ pages/chicaqhubo/index.js
@@ -27,7 +27,7 @@ export default function Perfiles({ girls }) {
             </Link>
             <div className={styles.botones}>
               <h3 className={styles.profile}>
-                <Link href='/chicaqhubo/dayana-sanchez'>
+                <Link href='/chicaqhubo/maria-lozano'>
                   <a>Ver la Chica Q'hubo del día</a>
                 </Link>
               </h3>
