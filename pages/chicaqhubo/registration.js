7ec6810571e77eb655872b620513012385dc3848
Camilo Galvis <deiby.galvis@laopinion.com.co>
2022-02-04 08:41:35 -0500

    actualizando url btn ver chica qhubo del dia

--- pages/chicaqhubo/registration.js
+++ pages/chicaqhubo/registration.js
@@ -50,7 +50,7 @@ export default function Home() {
           </Link>
             <div className={styles.botones}>
               <h3 className={styles.profile}>
-                <Link href='/chicaqhubo/dana-garcia'>
+                <Link href='/chicaqhubo/alejandra-perez'>
                   <a>Ver la Chica Q'hubo del día</a>
                 </Link>
               </h3>
