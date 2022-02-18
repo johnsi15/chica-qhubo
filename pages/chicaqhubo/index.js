cbe3376ed00ab57cbd0d2fec2bc0ec1d93253863
Camilo Galvis <deiby.galvis@laopinion.com.co>
2022-02-18 14:30:18 -0500

    actualizando url btn ver chica qhubo

--- pages/chicaqhubo/index.js
+++ pages/chicaqhubo/index.js
@@ -27,7 +27,7 @@ export default function Perfiles({ girls }) {
             </Link>
             <div className={styles.botones}>
               <h3 className={styles.profile}>
-                <Link href='/chicaqhubo/karina-moreno'>
+                <Link href='/chicaqhubo/marguijuliet-mendoza'>
                   <a>Ver la Chica Q'hubo del día</a>
                 </Link>
               </h3>
--- pages/chicaqhubo/registration.js
+++ pages/chicaqhubo/registration.js
@@ -50,7 +50,7 @@ export default function Home() {
           </Link>
             <div className={styles.botones}>
               <h3 className={styles.profile}>
-                <Link href='/chicaqhubo/karina-moreno'>
+                <Link href='/chicaqhubo/marguijuliet-mendoza'>
                   <a>Ver la Chica Q'hubo del día</a>
                 </Link>
               </h3>
