eab7f7da5ffaefd2574446e5549cc1acbe8cef3e
Camilo Galvis <deiby.galvis@laopinion.com.co>
2022-03-31 17:38:13 -0500

    cambiando link btn chica qhubo del día

--- pages/chicaqhubo/index.js
+++ pages/chicaqhubo/index.js
@@ -27,7 +27,7 @@ export default function Perfiles({ girls }) {
             </Link>
             <div className={styles.botones}>
               <h3 className={styles.profile}>
-                <Link href='/chicaqhubo/johanna-obando'>
+                <Link href='/chicaqhubo/viviana-varela'>
                   <a>Ver la Chica Q'hubo del día</a>
                 </Link>
               </h3>
--- pages/chicaqhubo/registration.js
+++ pages/chicaqhubo/registration.js
@@ -50,7 +50,7 @@ export default function Home() {
           </Link>
             <div className={styles.botones}>
               <h3 className={styles.profile}>
-                <Link href='/chicaqhubo/johanna-obando'>
+                <Link href='/chicaqhubo/viviana-varela'>
                   <a>Ver la Chica Q'hubo del día</a>
                 </Link>
               </h3>
